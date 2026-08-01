0f6bf9343ac005bb8b72687f9ee3af407c1cdedf
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-01 15:36:54 +0100

    test : testing MOD and POW macros

--- include/utils.h
+++ include/utils.h
@@ -68,3 +68,39 @@
     bigra9m_print(d);\
     printf("=========================================================\n");\
 } while (0);
+
+#define TEST_POWER_EXPO(a_str , b_str) do\
+{   printf("================ TESTING POWER ================\n" );\
+    BigInt a , b , c  , d;\
+    bigra9m_assign_str(&a , a_str);\
+    bigra9m_assign_str(&b , b_str);\
+    bigra9m_init(&c );\
+    printf("a : \n" );\
+    bigra9m_print(a);\
+    printf("b : \n" );\
+    bigra9m_print(b);\
+    bigra9m_pow(a , b , &c );\
+    printf("result : \n" );\
+    bigra9m_print(c);\
+    printf("=========================================================\n");\
+} while (0);
+
+
+#define TEST_MODULO(a_str , b_str) do\
+{   printf("================ TESTING MODULO ================\n" );\
+    BigInt a , b , c  , d;\
+    bigra9m_assign_str(&a , a_str);\
+    bigra9m_assign_str(&b , b_str);\
+    bigra9m_init(&c );\
+    bigra9m_init(&d );\
+    printf("a : \n" );\
+    bigra9m_print(a);\
+    printf("b : \n" );\
+    bigra9m_print(b);\
+    bigra9m_mod(a , b  , &d );\
+    printf("result : \n" );\
+    bigra9m_print(d);\
+    printf("=========================================================\n");\
+} while (0);
+
+

3bb38ae5c66629cf6f18a42e2db8dbe649b2ba83
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-13 18:03:15 +0100

    style: just better styling

--- script.sh
+++ script.sh
@@ -5,8 +5,11 @@
 prgrm=$1
 file_name=$2
 
-# echo $prgrm
-# echo $file_name
+passed=0
+failed=0
+
+NUMBER_OF_TESTS=1000
+
 
 standard_path="./tests/output/bin/"
 test_c_prgrm_name="run_tests_main"
@@ -16,12 +19,12 @@ full_c_path="$standard_path$test_c_prgrm_name"
 full_py_path="$test_python_prgrm_name"
 python_interpreter="python3"
 
-operation=" MUL "
+operation=" sub "
 # echo $full_path
 pwd
-for i in {1..1000}; do
+for ((i=0; i<$NUMBER_OF_TESTS; i++)) do
     NUM1=$(( (SRANDOM  ) + 4))
-    NUM2=$(( (SRANDOM % NUM1) + 2 ))
+    NUM2=$(( (SRANDOM % (NUM1 / 200 + 1)) + 2 ))
 
     echo "================ test Number: $i =============================="
     echo "num1: $NUM1"
@@ -42,13 +45,19 @@ for i in {1..1000}; do
 
     if [[ "$result_in_c" == "$result_in_py" ]]; then
         echo "✅ The strings match."  # trust me this is not AI i brought the emoji from https://emojipedia.org/check-mark-button
+        ((passed++))
     else
         echo "❌ The strings do not match." # same here twin https://emojipedia.org/cross-mark
+        ((failed++))
         exit 1
     fi    
     echo "==============================================================="
-    # exit 1
 done
 
+echo "==============================================================="
+echo "|| Total tests : $NUMBER_OF_TESTS                  "
+echo "|| ✅ passed tests : $passed                          "
+echo "|| ❌ failed tests : $failed                          "
+echo "==============================================================="
 
 # set -xe

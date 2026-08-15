71c3d98be66ec92c7664904de1ac11824fe88374
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-15 23:41:41 +0100

    actual useful utils (is this even worth it ?)

--- include/utils.h
+++ include/utils.h
@@ -1,100 +1,38 @@
 #include "../include/bigra9m.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-#define FANCY_DETAILED_PRINTING 0
-
-
-#define TEST_MULTIPLICATION(a_str , b_str) do\
-{   \
-    BigInt a , b , c ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_mul(a , b , &c);\
-    bigra9m_print(c);\
+#define MAX(a,b) (a>b ? a : b) 
+#define MIN(a,b) (a<b ? a : b) 
+// literally copying gmp style .-.
+// reallocating `_newsize` to `_ptr` and initializing the new allocated space to 0 .
+//  if realloc returns NULL we return -1 from wherever this macro is called . 
+#define BRZ_ALLOCATE_U64(_ptr , _newsize ) do \
+{\
+    _ptr = realloc(_ptr , _newsize*sizeof(uint64_t)) ;  \
+    if (_ptr == NULL) { \
+        fprintf(stderr , "ERROR: realloc failed sadly");    \
+        return -1;    \
+    }  \
 } while (0);
 
+static inline int nextPowerOfTwo(int n) {
+    if (n <= 1) return 2;
+    
+    int result = n - 1;
+    result |= result >> 1;
+    result |= result >> 2;
+    result |= result >> 4;
+    result |= result >> 8;
+    result |= result >> 16;
+    result |= result >> 32;
+    result++;
+    
+    // If n was already a power of 2, double the result
+    if (result == n) {
+        result <<= 1;
+    }
+    
+    return result;
+}
 
-#define TEST_ADDITION(a_str , b_str) do\
-{   \
-    BigInt a , b , c ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_add(a , b , &c);\
-    bigra9m_print(c);\
-} while (0);
-
-
-#define TEST_SUBTRACTION(a_str , b_str) do\
-{   \
-    BigInt a , b , c ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_sub(a , b , &c);\
-    bigra9m_print(c);\
-} while (0);
-
-
-#define TEST_DIVISION(a_str , b_str) do\
-{   \
-    BigInt a , b , c  , d;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_init(&c );\
-    bigra9m_init(&d );\
-    bigra9m_div(a , b , &c , &d);\
-    bigra9m_print(c);\
-} while (0);
-
-#define TEST_POWER_EXPO(a_str , b_str) do\
-{   \
-    BigInt a , b , c  , d;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_init(&c );\
-    bigra9m_pow(a , b , &c );\
-    bigra9m_print(c);\
-} while (0);
-
-
-#define TEST_MODULO(a_str , b_str) do\
-{   \
-    BigInt a , b , c  , d;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_init(&c );\
-    bigra9m_init(&d );\
-    bigra9m_mod(a , b  , &d );\
-    bigra9m_print(d);\
-} while (0);
-
-#define TEST_MODULO2(a_str , b_str , c_str) do\
-{   \
-    BigInt a , b , c  , d;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-    bigra9m_assign_str(&c , c_str);\
-    bigra9m_init(&d );\
-    bigra9m_mod2(a , b ,c   , &d );\
-    bigra9m_print(d);\
-} while (0);
-
-#define TEST_EQ(a_str , b_str) do\
-{   \
-    BigInt a , b ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-} while (0);
-
-#define TEST_BG(a_str , b_str) do\
-{   \
-    BigInt a , b ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_assign_str(&b , b_str);\
-} while (0);
-
-#define TEST_MULTIPLICATION_uint64_t(a_str , _b) do\
-{   \
-    BigInt a  , c ;\
-    bigra9m_assign_str(&a , a_str);\
-    bigra9m_mul_uint64(a , _b , &c);\
-    bigra9m_print(c);\
-} while (0);

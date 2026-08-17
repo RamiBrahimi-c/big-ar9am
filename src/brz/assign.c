402a50880bc953f0d1a89cd665970eb77dde1b6e
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-17 17:26:48 +0100

    trust idk wth im doing here , basic definitions of init and assign function and need to get checked AGAIN and AGAIN!!!!!!!!

--- src/brz/assign.c
+++ src/brz/assign.c
@@ -1,7 +1,12 @@
 #include "../../include/bigra9m.h"
+#include "../../include/utils.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+// TODO: these things are messy as hell and need to get checked AGAIN !!!!!!!!!!!
 
 // NOTE : not configured yet with dynammic approach
+// NOTE : these function should be treated as a `init` ???
 
 
 static int is_num(const char a) {
@@ -12,7 +17,6 @@ static int is_num(const char a) {
 
 
 // accepts numbers that are like (-9.../913...) aka starts with number OR numeral digit , yet needs better handling  
-// NOTE : works only with base 100   
 // TODO : handle input properly like in bizarre cases such as "4545-+565"
 // TODO : replace atoi() with strtol ... 
 void bigra9m_assign_str(BigInt *a , const char *num_str) {
@@ -20,12 +24,14 @@ void bigra9m_assign_str(BigInt *a , const char *num_str) {
     uchar_t *num_str_clean = strdup(num_str) ;
     // cuz we might increment `num_str_clean` therefore it wont point exactly to the first allocated byte 
     uchar_t *temp = num_str_clean ;
+    int sign = 1 ; 
     if (num_str[0]== '-' && is_num(num_str[1]))
     {
-        a->length = -1 ;
+        sign = -1 ; 
+        // a->length = -1 ;
         num_str_clean = num_str_clean + 1 ;  
     } else if (is_num(num_str[0]))  {
-        a->length = 1 ;
+        // a->length = 1 ;
         // num_str_clean = num_str  ;  
         
     } else {
@@ -37,48 +43,33 @@ void bigra9m_assign_str(BigInt *a , const char *num_str) {
     
 
     size_t len = strlen((const char*) num_str_clean) ;
+    // for now
+    int base = 10 ; 
 
-    size_t j = 0 ;
-    if (len % 2 ==0)
+    // bigra9m_init(a) ; 
+    bigra9m_assign_uint64_t(a , 0) ; 
+
+    for (size_t i = 0; i < len; i++)
     {
-        j = len/2 ;
-        a->length *= j ; 
-        j-- ; 
-        for (size_t i = 0; i < len; i+=2)
-        {
-            uchar_t buffer[2] ; 
-            memcpy(buffer , num_str_clean + i , sizeof(uchar_t)*2) ;
-            a->nums[j] = atoi(buffer ) ;
-            j-- ; 
-        }
-        
-    } else {
-        j = len/2 +1;;
-        a->length *= j ; 
-        j-- ; 
-        uchar_t buffer[2] ; 
-        buffer[1] = 0  ; 
-        memcpy(buffer   , num_str_clean  , sizeof(uchar_t)) ;
-        a->nums[j] = atoi(buffer) ;
-        j-- ; 
-        
-        for (size_t i = 1; i < len; i+=2)
-        {
-            uchar_t buffer[2] ; 
-            memcpy(buffer , num_str_clean + i , sizeof(uchar_t)*2) ;
-            a->nums[j] = atoi(buffer ) ;
-            j-- ; 
-        }        
+        printf("i=%ld\n" , i) ; 
+        // read one character or one digit
+        uchar_t c = num_str_clean[i] ; 
+        uint64_t val = c - '0' ; 
+        printf("val : %lu\n" , val) ; 
+        bigra9m_mul_uint64(a , base , a ) ;
+        bigra9m_add_1(*a , val , a ) ;
     }
+    a->length *= sign ; 
+
     free(temp);
 }
 
 
 
 
-void bigra9m_assign_uint64_t(BigInt *a , uint64_t x) {
+int bigra9m_assign_uint64_t(BigInt *a , uint64_t x) {
 
-    bigra9m_init(a);
+    // bigra9m_init(a);
     int i=0;
     int c;
     while (x > 0)
@@ -87,8 +78,24 @@ void bigra9m_assign_uint64_t(BigInt *a , uint64_t x) {
         a->nums[i] = c;
         i++;
         x /= BASE ;
+        a->length = i ;
+        if (a->capacity < i*2)
+        {
+            a->capacity = nextPowerOfTwo(a->capacity) ; 
+            // printf("reallocating with %d \n" , a->capacity) ; 
+            BRZ_ALLOCATE_U64(a->nums , a->capacity )
+            /* a->nums = realloc(a->nums , a->capacity * sizeof(uint64_t)) ;  
+            if (a->nums == NULL) { 
+                fprintf(stderr , "ERROR: realloc failed sadly");    
+                return -1;    
+            }  
+            printf("array start at : %p \n" , a->nums) ; 
+            printf("about to set %d bytes to 0 starting from %p \n" ,a->capacity -  a->length , a->nums + a->length ) ;
+
+            printf("reallocating???\n") ;  */
+        }
+        
     }
-    a->length = i ;
 
     for (size_t j = 0; j < i; j++)
     {
@@ -100,10 +107,22 @@ void bigra9m_assign_uint64_t(BigInt *a , uint64_t x) {
     
 }
 
-
+// IMPORTANT : here we pretend like a went through init before
 void bigra9m_assign(BigInt *a , BigInt b  ) {
 
-    memcpy(a , &b , sizeof(BigInt)) ; 
+    a->length = b.length ; 
+    // a->capacity = b.capacity ; 
+    if (a->capacity < b.capacity)
+    {
+        a->capacity = b.capacity ; 
+        BRZ_ALLOCATE_U64(a->nums , a->capacity)
+        memset(a->nums + abs(a->length), 0x0 , sizeof(uint64_t)*(a->capacity - abs(a->length))) ; 
+        
+    }
+    memcpy(a->nums , b.nums , sizeof(uint64_t)*abs(b.length)) ; 
+    
+    // memcpy(a , &b , sizeof(BigInt)) ; 
+
     // a->length = b.length ; 
 }
 
--- src/brz/init.c
+++ src/brz/init.c
@@ -1,12 +1,20 @@
+#ifndef INIT_C
+#define INIT_C
+
 #include "../../include/bigra9m.h"
+#include "../../include/utils.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+
+// TODO: these things are messy as hell and need to get checked AGAIN !!!!!!!!!!!
+
 
 // NOTE : is configured with dynammic approach
 
 int bigra9m_init(BigInt *a) {
     a->length = 0 ;
-    a->capacity = 16 ; 
+    a->capacity = 4 ; 
     a->nums = malloc(sizeof(uint64_t)*a->capacity) ; 
     if (a->nums == NULL)
     {
@@ -17,3 +25,102 @@ int bigra9m_init(BigInt *a) {
     memset(a->nums , 0 , a->capacity *sizeof(uint64_t)) ;
     return 0 ;
 }
+
+
+int bigra9m_inits(BigInt *a , ...) {
+
+    va_list  ap;
+
+    va_start (ap, a);
+
+    do {
+        bigra9m_init(a) ; 
+        a = va_arg (ap, BigInt *);
+    }
+    while (a != NULL);
+
+    va_end (ap);    
+
+    return 0 ;
+}
+
+int bigra9m_init_assign(BigInt *a , BigInt b  ) {
+    bigra9m_init(a) ; 
+    a->length = b.length ; 
+    // a->capacity = b.capacity ; 
+    if (a->capacity < b.capacity)
+    {
+        a->capacity = b.capacity ; 
+        BRZ_ALLOCATE_U64(a->nums , a->capacity) 
+        memset(a->nums + abs(a->length), 0x0 , sizeof(uint64_t)*(a->capacity - abs(a->length))) ; 
+        
+    }
+    memcpy(a->nums , b.nums , sizeof(uint64_t)*abs(b.length)) ; 
+    
+    // memcpy(a , &b , sizeof(BigInt)) ; 
+
+    // a->length = b.length ; 
+}
+static int is_num(const char a) {
+    return a >= '0' && a <= '9' ; 
+}
+
+
+// accepts numbers that are like (-9.../913...) aka starts with number OR numeral digit , yet needs better handling  
+// TODO : handle input properly like in bizarre cases such as "4545-+565"
+// TODO : replace atoi() with strtol ... 
+void bigra9m_init_str(BigInt *a , const char *num_str) {
+    
+    uchar_t *num_str_clean = strdup(num_str) ;
+    // cuz we might increment `num_str_clean` therefore it wont point exactly to the first allocated byte 
+    uchar_t *temp = num_str_clean ;
+
+    int sign = 1 ; 
+    if (num_str[0]== '-' && is_num(num_str[1]))
+    {
+        // a->length = -1 ;
+        num_str_clean = num_str_clean + 1 ;
+        sign = -1 ;  
+    } else if (is_num(num_str[0]))  {
+        // a->length = 1 ;
+        // num_str_clean = num_str  ;  
+        
+    } else {
+        // a->length = 0 ;
+        bigra9m_init(a) ; 
+        free(num_str_clean) ; 
+        return ; 
+    }
+    
+
+    size_t len = strlen((const char*) num_str_clean) ;
+    // for now
+    int base = 10 ; 
+
+    bigra9m_init(a) ; 
+    bigra9m_assign_uint64_t(a , 0) ; 
+
+    for (size_t i = 0; i < len; i++)
+    {
+        // printf("i=%ld\n" , i) ; 
+        // read one character or one digit
+        if (!is_num(num_str_clean[i]))
+            continue;
+        uchar_t c = num_str_clean[i] ; 
+        uint64_t val = c - '0' ; 
+        // printf("val : %lu\n" , val) ; 
+        bigra9m_mul_uint64(a , base , a ) ;
+        // printf("not here\n") ; 
+        bigra9m_add_1(*a , val , a ) ;
+    }
+    a->length *= sign ; 
+
+    free(temp) ; 
+
+
+
+
+}
+
+
+#endif

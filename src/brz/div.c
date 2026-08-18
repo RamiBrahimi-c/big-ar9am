8770c9f2d4e2c28f23f669d06ad5500f017d169d
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-18 23:28:17 +0100

    feat && fix : knuth's for when n=1 and fixed for the memory approach

--- src/brz/div.c
+++ src/brz/div.c
@@ -1,7 +1,12 @@
 #include "../../include/bigra9m.h"
+#include "../../include/utils.h"
 
 // NOTE : not configured yet with dynammic approach
+// NOTE : more tests needed !!!!
+// NOTE IMPORTANT : i changed the signature of knuth's algorithm cuz this one `knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Reminder)`
+// caused a double free somehow for a reason i STILL DO NOT KNOW yet ... 
 
+#define DEBUG_KNUTHS 0
 
 static void repeated_subtraction_division(BigInt N , BigInt D ,BigInt *Qoeff , BigInt *Reminder ) {
     if (D.length ==0)
@@ -16,7 +21,7 @@ static void repeated_subtraction_division(BigInt N , BigInt D ,BigInt *Qoeff , B
     bigra9m_assign_str(&temp , "1") ; 
     while (bigra9m_isBiggerThanNum(R , D))
     {
-        bigra9m_sub(R , D  , &temp2) ; 
+        bigra9m_sub(&R , &D  , &temp2) ; 
         bigra9m_add(Q , temp  , &Q) ; 
         bigra9m_assign(&R , temp2) ; 
         
@@ -31,9 +36,80 @@ static void repeated_subtraction_division(BigInt N , BigInt D ,BigInt *Qoeff , B
 }
 
 
+static void brz_assign_range(BigInt *a ,BigInt b , int start , int end ) {
+    if (end < start || start < 0 || end < 0 || b.length < end + 1)
+    {
+        return ; 
+    }
+    if (a->capacity < end - start + 1)
+    {
+        a->capacity = nextPowerOfTwo(end - start + 1) ; 
+        BRZ_ALLOCATE_U64(a->nums , a->capacity)
+        memset(a->nums + abs(a->length) , 0x0 , sizeof(uint64_t)*(a->capacity - abs(a->length))) ; 
+    }
+
+    int k = 0 ; 
+    for (size_t i = start; i <= end; i++)
+    {
+        a->nums[k] = b.nums[i] ; 
+        k++ ; 
+    }
+    a->length = k ; 
+    
+    
+}
+
+
+// when n == 1 
+static void simple_knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Reminder) {
+    printf("simple knuth !!!") ; 
+    int n = U.length  ; 
+    int j = n -1 ; 
+    uint64_t r = 0 ; 
+    BigInt temp_U , temp_V ;
+    // printf("calling init from simple_knuths_algorithm_d for temp_U") ; 
+    bigra9m_init_assign(&temp_U , U) ; 
+    // printf("calling init from simple_knuths_algorithm_d for temp_V") ; 
+    bigra9m_init_assign(&temp_V , V) ; 
+    
+    
+    do {
+        // step 3 : Calculate q^
+
+        uint64_t q_hat = (r*BASE + U.nums[j]) / V.nums[0] ; 
+        r = (r*BASE + U.nums[j]) % V.nums[0] ; 
+        Qoeff->nums[j] = q_hat ; 
+        j-- ; 
+
+    } while (j>=0) ; 
+
+    // bigra9m_clears(&temp , &temp2 , NULL) ; 
+
+
+    while (!bigra9m_is_clean_lastdigit(*Qoeff))
+    {
+        bigra9m_clean_lastdigit(Qoeff) ; 
+    }
+    
+    while (!bigra9m_is_clean_lastdigit(*Reminder))
+    {
+        bigra9m_clean_lastdigit(Reminder) ; 
+    }
+    
+    bigra9m_mod2(temp_U , temp_V , *Qoeff , Reminder) ; 
+
+    // printf("calling clears from simple_knuths_algorithm_d for temp_V , temp_U") ; 
+    bigra9m_clears(&temp_U , &temp_V , NULL) ; 
+}
+
+
 // fuck this algorithm
 // TODO : this algo stills need to do the length correctly plus calculating the reminder ..
-static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Reminder) {
+void knuths_algorithm_d(BigInt *U , BigInt *V ,BigInt *Qoeff , BigInt *Reminder) {
+    // BigInt U , V ; 
+    // bigra9m_init_assign(&U , U_origin ) ; 
+    // bigra9m_init_assign(&V , V_origin ) ; 
+    
     #if DEBUG_KNUTHS
         printf("knuth's algorithm here !!!!!\n");
         printf("DIVIDEND : \n") ; 
@@ -41,23 +117,35 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
         printf("DIVISOR : \n") ; 
         bigra9m_print(V); 
     #endif
-    uint64_t D = (BASE-1) / V.nums[V.length-1] ;
+    uint64_t D = (BASE-1) / V->nums[V->length-1] ;
     BigInt temp_U , temp_V ;
-    bigra9m_assign(&temp_U , U) ; 
-    bigra9m_assign(&temp_V , V) ; 
+    // bigra9m_inits()
     
-    int m = U.length - V.length ; 
+    // printf("calling init from knuths_algorithm_d for temp_U") ; 
+    bigra9m_init_assign(&temp_U , *U) ; 
+    // printf("calling init from knuths_algorithm_d for temp_V") ; 
+    bigra9m_init_assign(&temp_V , *V) ; 
+    
+    int m = U->length - V->length ; 
     int j = m ;
-    int n = V.length ;
+    int n = V->length ;
     Qoeff->length = m +1; 
+    if (n==1)
+    {
+        simple_knuths_algorithm_d(*U , *V , Qoeff , Reminder) ; 
+        // printf("calling clears from knuths_algorithm_d for temp_V temp_U") ; 
 
+        bigra9m_clears(&temp_U , &temp_V , NULL) ; 
+        return ; 
+    }
+    
     // step 1 : [Normalize]
-    while(V.nums[V.length-1] < BASE/2) {
+    while(V->nums[V->length-1] < BASE/2) {
         // uint64_t d = (uint64_t) (BASE-1) / V.nums[V.length-1] ; 
         uint64_t d = 2 ; 
         
-        bigra9m_mul_uint64(U , d , &U) ;
-        bigra9m_mul_uint64(V , d , &V) ;
+        bigra9m_mul_uint64(U , d , U) ;
+        bigra9m_mul_uint64(V , d , V) ;
     }  
     
     #if DEBUG_KNUTHS
@@ -74,24 +162,28 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
         NOTE in this phase , U is supposed to get added a 0 on its left
         make sure to have enough allocated space ...
     */
-    m = U.length - V.length ; 
-    n = V.length ; 
+    m = U->length - V->length ; 
+    n = V->length ; 
     Qoeff->length = m +1; 
 
     
     // step 2 : [Initilize]
     j = m ; 
 
+    BigInt temp , temp2;
+    
+    // printf("calling init from knuths_algorithm_d for temp:%p , temp2:%p" , &temp , &temp2) ; 
+    bigra9m_inits(&temp , &temp2 , NULL) ; 
     do {
         // step 3 : Calculate q^
 
-        uint64_t q_hat = (U.nums[j+n]*BASE + U.nums[j+n-1]) / V.nums[n-1] ; 
-        uint64_t r_hat = (U.nums[j+n]*BASE + U.nums[j+n-1]) % V.nums[n-1] ; 
+        uint64_t q_hat = (U->nums[j+n]*BASE + U->nums[j+n-1]) / V->nums[n-1] ; 
+        uint64_t r_hat = (U->nums[j+n]*BASE + U->nums[j+n-1]) % V->nums[n-1] ; 
 
-        while (q_hat == BASE ||  q_hat * V.nums[n-2] > BASE*r_hat + U.nums[j+n-2])
+        while (q_hat == BASE ||  q_hat * V->nums[n-2] > BASE*r_hat + U->nums[j+n-2])
         {
             q_hat-- ; 
-            r_hat += V.nums[n-1] ; 
+            r_hat += V->nums[n-1] ; 
 
             if (r_hat >= BASE)
             {
@@ -102,32 +194,18 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
 
         // step 4 : [Multiply and Subtract]
 
-        BigInt temp , temp2;
         int k = 0 ;  
-        for (size_t i = j; i < j + n + 1; i++)
-        {
-            temp.nums[k] = U.nums[i] ;
-            k++  ; 
-        }
-        // temp.length = j + n + 1 ; 
-        temp.length = k ; 
+
+        brz_assign_range(&temp , *U , j , j+ n ) ; 
         // temp now has digits of (U) from (j) to (n+j)
         
-        bigra9m_init(&temp2) ; 
+        bigra9m_assign_uint64_t(&temp2 , 0) ; 
 
         bigra9m_mul_uint64(V , q_hat , &temp2) ; 
-        bigra9m_sub(temp , temp2 , &temp ) ; 
+        bigra9m_sub(&temp , &temp2 , &temp ) ; 
 
-        // if (!bigra9m_is_negative(temp))
+        brz_assign_range(U , temp , j , j + n) ; 
         
-            k=0 ; 
-            for (size_t i = j; i < j + n + 1; i++)
-            {
-                U.nums[i] = temp.nums[k]  ; 
-                k++ ; 
-
-            }
-        // U.length = 
 
         // step 5 : [Test Reminder]
 
@@ -139,23 +217,19 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
             // step 6 : [Add back]
             #if DEBUG_KNUTHS
 
-                printf("panic\n"); 
-                // for debugging ...
-                printf("this happened when U : \n"); 
-                bigra9m_print(temp_U) ; 
-                printf("and V : \n"); 
-                bigra9m_print(temp_U) ; 
+            printf("panic\n"); 
+            // for debugging ...
+            printf("this happened when U : \n"); 
+            bigra9m_print(temp_U) ; 
+            printf("and V : \n"); 
+            bigra9m_print(temp_U) ; 
             #endif
             // exit(64) ;
             Qoeff->nums[j]-- ; 
-            bigra9m_add(V , temp , &temp) ;
+            bigra9m_add(*V , temp , &temp) ;
+            
 
-            k=0 ; 
-            for (size_t i = j; i < j +  n + 1; i++)
-            {
-                U.nums[i] = temp.nums[k]  ; 
-                k++ ; 
-            }
+            brz_assign_range(&U , temp , j , j + n) ; 
 
         }
         // step 7 : [Loop on j]
@@ -164,14 +238,10 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
     } while (j>=0) ; 
     
     
-
-
-    // bigra9m_mul(U , D , U) ;
-    // bigra9m_mul(V , D , V) ;
     
-    // bigra9m_assign(&D , );
-    //     Set Q̂ to (U[n+j] × B + U[n−1+j]) ÷ V[n−1]; 
-    //     Set R̂ to (U[n+j] × B + U[n−1+j]) % V[n−1]; 
+    // printf("calling clears from knuths_algorithm_d for temp %p , temp2 %p" , &temp , &temp2) ; 
+    bigra9m_clears(&temp , &temp2 , NULL) ; 
+
 
     while (!bigra9m_is_clean_lastdigit(*Qoeff))
     {
@@ -188,6 +258,7 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
     printf("quoeff : ????\n");
     bigra9m_print(*Qoeff);
     #endif
+    // printf("calling mod2\n") ; 
     bigra9m_mod2(temp_U , temp_V , *Qoeff , Reminder) ; 
     
     #if DEBUG_KNUTHS
@@ -195,123 +266,97 @@ static void knuths_algorithm_d(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Remi
     bigra9m_print(*Reminder);
     #endif
 
-}
-/*    uint64_t normalization_factor = 0 ; 
-    BigInt big_2 ; 
-    bigra9m_assign_str(&big_2 , "2") ; 
 
-
-    // first step normalizing ????
-    
-    while (V.nums[V.length-1] < BASE / 2  )
-    {
-        bigra9m_mul(U , big_2 , &U) ; 
-        bigra9m_mul(V , big_2 , &V) ; 
-        normalization_factor++ ; 
-    }
-    
-
-    
-    for (size_t i = 0; i < V.length - 1; i++)
-    {
-        U.nums[i] = V.nums[i] = 0 ; 
-    }
-    
-    uint64_t ratio = (U.nums[U.length-1] * (BASE-1) + U.nums[U.length-1]   ) ;
-    ratio = ratio < BASE -1 ? ratio : BASE-1 ; 
-    ratio /= V.nums[V.length-1] ; 
-
-    #ifdef DEBUG_KNUTHS
-        printf("printing after normalizing : \n") ; 
-        bigra9m_print(U); 
-        bigra9m_print(V); 
-
-        printf("ratio : %lu \n" , ratio ) ; 
-    #endif
-
-    BigInt r_hat , q_hat; 
-    bigra9m_init(&r_hat) ; 
-    bigra9m_init(&q_hat) ; 
-    bigra9m_assign_uint64_t(&q_hat ,ratio ) ;
-    
-    #ifdef DEBUG_KNUTHS
-    printf("q hat :  "  ) ; 
-    bigra9m_print(q_hat) ; 
-    #endif    
-    bigra9m_mod2(U , V , q_hat , &r_hat ) ; 
-    
-    #ifdef DEBUG_KNUTHS
-        printf("r hat : "  ) ; 
-        bigra9m_print(r_hat) ; 
-    #endif
-
-    BigInt temp1 , temp2 , temp3 , big1 , big6 , big7; 
-    bigra9m_init(&temp1) ; 
-    bigra9m_init(&temp2) ; 
-    bigra9m_assign_str(&big1 , "1") ; 
-    bigra9m_assign_uint64_t(&big6 , temp_U.nums[0]) ; 
-    bigra9m_assign_uint64_t(&big7 , temp_V.nums[0] ) ; 
-
-
-    bigra9m_mul(q_hat , big7 , &temp1) ; 
-    bigra9m_add(r_hat , big6 , &temp2) ; 
-
-    #ifdef DEBUG_KNUTHS
-        printf("hi!!!\n");
-        bigra9m_print(big6);
-        bigra9m_print(big7);
-        bigra9m_print(temp1);
-        bigra9m_print(temp2);
-    #endif
-        // temp2.length-- ; 
-        printf("hi!!! is bigger :%d \n" , bigra9m_isStrictlyBiggerThanNum(temp1 , temp2));
-    
-    while (bigra9m_isStrictlyBiggerThanNum(temp1 , temp2))
-    {
-        printf("hi???\n");
-        bigra9m_sub(q_hat , big1 , &temp3);
-        bigra9m_assign(&q_hat  , temp3);
-
-        bigra9m_print(temp3) ; 
-
-        bigra9m_init(&temp1) ; 
-        bigra9m_mul(q_hat , big7 , &temp1) ; 
-
-        bigra9m_mod2(U , V , temp3 , &r_hat ) ; 
-        bigra9m_add(r_hat , big6 , &temp2) ; 
-
-    }
-    
-    bigra9m_assign(Qoeff , temp3) ; 
-
-    bigra9m_mod2(temp_U ,temp_V , temp3 , Reminder);
-    // bigra9m_assign(Reminder , r_hat) ; 
-    printf("normalization factor : %lu\n" , normalization_factor) ;
-
-    return ; 
-    
-
-
-    #ifdef DEBUG_KNUTHS
-        printf("m=%d\n" , m) ;
-    #endif
-    #endif
-*/
-static int bigra9m_is_normalized(BigInt a) {
-
-
-    return 0;
+    bigra9m_clears(&temp_U , &temp_V , NULL) ; 
 }
 
 
-static void naive_division(BigInt U , BigInt V ,BigInt *Qoeff , BigInt *Reminder) {
+
+void bigra9m_div(BigInt *a , BigInt *b , BigInt *c ,  BigInt *d) {
+    if (bigra9m_is_zero(*b) ) {
+        fprintf(stderr , "ERROR : trying to devide on 0 !\n" ) ;
+        return ; 
+    }
+
+    if (bigra9m_is_zero(*a) )
+    {
+        bigra9m_assign_uint64_t(c , 0) ; 
+        bigra9m_assign(d , *a ) ; 
+
+        return ; 
+    }
     
+    int sign = 1 ; 
 
-}
+    if (a->length * b->length < 0)
+    {
+        sign = -1 ; 
+    }
+
+    if (a==c || b==c)
+    {
+        BigInt  temp_a , temp_b ; 
+        
+        // printf("calling init from bigra9m_div for temp_a %p , temp_b %p" , &temp_a , &temp_b) ; 
+        bigra9m_inits(  &temp_a , &temp_b  , NULL) ; 
+
+        bigra9m_assign(&temp_a , *a) ; 
+        bigra9m_assign(&temp_b , *b) ; 
+
+        // c gotta be initilized 
+        memset(c->nums , 0x0 , sizeof(uint64_t)*c->capacity) ; 
+        
+        // lets allocate for our result ...
+
+        if (nextPowerOfTwo(abs(temp_a.length)-abs(temp_b.length) + 1 ) >  c->capacity  ) {
+            c->capacity = nextPowerOfTwo(abs(temp_a.length)-abs(temp_b.length) + 1) ;
+            BRZ_ALLOCATE_U64(c->nums , c->capacity ) 
+            memset(c->nums + abs(c->length)  , 0 , sizeof(uint64_t)*(c->capacity -  abs(c->length) )) ;
+
+        }
+
+        // basecase_multiplication(temp_a , temp_b , c) ; 
+        knuths_algorithm_d(&temp_a , &temp_b , c , d) ; 
+
+        c->length *= sign ; 
+        
+        // printf("calling clear from bigra9m_div for temp_a temp_b") ; 
+        bigra9m_clears(&temp_a , &temp_b , NULL) ; 
+        
+    } else {
+        BigInt   temp_a , temp_b ; 
+        // printf("else calling init from bigra9m_div for temp_a %p , temp_b %p" , &temp_a , &temp_b) ; 
+        bigra9m_inits( &temp_a , &temp_b , NULL) ; 
+
+        bigra9m_assign(&temp_a , *a) ; 
+        bigra9m_assign(&temp_b , *b) ; 
+        
+        // c gotta be initilized 
+        // memset(c->nums , 0x0 , sizeof(uint64_t)*c->capacity) ; 
+        // printf("uh?\n") ; 
+        
+        // lets allocate for our result ...
+        if (nextPowerOfTwo(abs(a->length)-abs(b->length) + 1 ) >  c->capacity  ) {
+            c->capacity = nextPowerOfTwo(abs(a->length)-abs(b->length) + 1) ;
+            BRZ_ALLOCATE_U64(c->nums , c->capacity ) 
+            memset(c->nums + abs(c->length)  , 0 , sizeof(uint64_t)*(c->capacity -  abs(c->length) )) ;
+
+        }
+
+        knuths_algorithm_d(&temp_a , &temp_b , c , d) ; 
+        // printf("======================== knuth done \n") ; 
+        // basecase_multiplication(*a , *b , c) ; 
+        c->length *= sign ; 
+        // printf("what?\n") ;
+        
+        // printf("calling clear from bigra9m_div for temp_a %p temp_b %p" , &temp_a , &temp_b) ; 
+        // printf("temp_a.nums = %p \n" , temp_a.nums) ;
+        // bigra9m_clear(&temp_a ) ; 
+        bigra9m_clears(&temp_a , &temp_b , NULL) ; 
+    }
 
 
-void bigra9m_div(BigInt a , BigInt b , BigInt *c ,  BigInt *d) {
-    knuths_algorithm_d(a , b , c , d) ; 
+    // knuths_algorithm_d(a , b , c , d) ; 
     // repeated_subtraction_division(a , b , c , d) ; 
 
 }

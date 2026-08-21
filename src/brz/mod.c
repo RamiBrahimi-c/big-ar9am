7d0000bd2aee7c620641b10b348494e6c411681f
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-21 17:29:39 +0100

    idk bro , it works thats it

--- src/brz/mod.c
+++ src/brz/mod.c
@@ -1,20 +1,19 @@
 #include "../../include/bigra9m.h"
 
-// NOTE : not configured yet with dynammic approach
-
+// NOTE : configured with dynammic approach
+// #define DEBUG_MOD2 0
 
 void bigra9m_mod2(BigInt dividend , BigInt divisor , BigInt quoeff , BigInt *c) {
     BigInt temp , temp2; 
-
-    bigra9m_init(&temp) ;
-    bigra9m_init(&temp2) ;
+    // printf("calling init from bigra9m_mod2 for temp %p , temp2 %p \n" , &temp , &temp2) ; 
+    bigra9m_inits(&temp , &temp2 , NULL) ;
     #ifdef DEBUG_MOD2
         printf("multiplying : ");
         bigra9m_print(divisor) ; 
         printf("and : ");
         bigra9m_print(quoeff) ; 
     #endif
-    bigra9m_mul(divisor , quoeff , &temp) ; 
+    bigra9m_mul(&divisor , &quoeff , &temp) ; 
     #ifdef DEBUG_MOD2
 
         printf("=  ");
@@ -26,16 +25,22 @@ void bigra9m_mod2(BigInt dividend , BigInt divisor , BigInt quoeff , BigInt *c) 
         bigra9m_print(temp) ; 
     #endif    
     
-    bigra9m_sub(dividend , temp , c);
+    bigra9m_sub(&dividend , &temp , c);
     
     #ifdef DEBUG_MOD2
-        printf("=  ");
-        bigra9m_print(*c) ; 
+    printf("=  ");
+    bigra9m_print(*c) ; 
     #endif
-}
+    // printf("calling clear from bigra9m_mod2 for temp %p , temp2 %p \n" , &temp , &temp2) ; 
+    bigra9m_clears(&temp , &temp2 , NULL) ;
 
+
+}
+#define DEBUG_MOD 1
 
 void bigra9m_mod(BigInt dividend , BigInt divisor , BigInt *c) {
     BigInt a ;
-    bigra9m_div(dividend , divisor ,&a , c ) ; 
+    bigra9m_init(&a) ; 
+    bigra9m_div(&dividend , &divisor ,&a , c ) ; 
+    bigra9m_clear(&a) ; 
 }

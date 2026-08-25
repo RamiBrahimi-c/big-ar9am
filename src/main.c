4249265bf697555f213ca3d8c38f5b0ca8fe50b2
RamiBrahimi-c <ramibrahimi02@gmail.com>
2026-08-25 23:58:46 +0100

    useless

--- src/main.c
+++ src/main.c
@@ -5,19 +5,12 @@
 
 #include "../include/bigra9m.h"
 
-#if 0
-    #define DEBUG
-#endif
+
+int main(int argc , char *argv[]) {
+
+    printf("hiii big ar9am >-<\n") ; 
 
 
-// int main(int argc , char *argv[]) {
 
-//     printf("hiii big ar9am >-<\n") ; 
-
-//     BigInt a;
-//     uint64_t x = 10254650; 
-//     bigra9m_assign_uint64_t(&a , x );
-//     printf("x=%lu\n" , x) ; 
-//     bigra9m_print(a);
-//     return 0 ; 
-// }
+    return 0 ; 
+}
